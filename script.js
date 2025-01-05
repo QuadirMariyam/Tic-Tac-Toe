4d5e9e173eff27204d3e2ea1c8adafe72599ff0e
Mariyam Abdul Quadir <quadirmariyam@gmail.com>
2025-01-05 20:36:35 +0530

    final commit
    Inspired by Apna College, the Tie function in this Tic-Tac-Toe is solely by me.

---
+++ script.js
@@ -0,0 +1,92 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newGame = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-div");
+let msg = document.querySelector("#msg");
+let count = 0;
+
+let turnO =  true; //if false = playerX, if ture = playerO
+
+const winPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+
+const resetGame = () => {
+    turnO = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+};
+
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if(turnO) {
+            box.innerText = "O";
+            turnO = false;
+        } else {
+            box.innerText = "X";
+            turnO = true;
+        }
+        box.disabled = true;
+        count++;
+        console.log(count);
+
+        checkWinner();
+    });
+});
+
+
+const disableBoxes = () => {
+    for(let box of boxes) {
+        box.disabled = true;
+    }
+};
+
+
+const enableBoxes = () => {
+    for(let box of boxes) {
+        box.disabled = false;
+        box.innerText = "";
+        count = 0;
+    }
+};
+
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes()
+};
+
+const checkWinner = () => {
+    for(let pattern of winPatterns) {
+        let pos1 = boxes[pattern[0]].innerText;
+        let pos2 = boxes[pattern[1]].innerText;
+        let pos3 = boxes[pattern[2]].innerText;
+        console.log(pattern);
+        console.log([pos1, pos2, pos3])
+
+        if(pos1 != "" && pos2 != "" && pos3 != "") {
+            if(pos1 === pos2 && pos2 === pos3) {
+                showWinner(pos1);
+            } 
+
+            if(count == 9 && [pos1, pos2, pos3] != pattern) {
+                msg.innerText = `Oops! Its a tie`;
+                msgContainer.classList.remove("hide");
+                disableBoxes()
+            }
+        }
+    }
+};
+
+newGame.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
